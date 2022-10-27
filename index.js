b332366978ba265e909bd1de6badd19af90add68
radoone <radoone@gmail.com>
2022-10-27 11:34:21 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ Array.prototype.random = function () {
   return this[Math.floor(Math.random() * this.length)];
 };
 
+
+
+
+
 const vrece100 = [...Array(100).keys()].map((i) => (i + 1) % 2);
 const vrece200 = [...Array(200).keys()].map((i) => (i + 1) % 2);
 
-let vrece100_rovnake=0;
-let vrece100_rozne=0;
+let vrece100_rovnake = 0;
+let vrece100_rozne = 0;
 
-let vrece200_rovnake=0;
-let vrece200_rozne=0;
+let vrece200_rovnake = 0;
+let vrece200_rozne = 0;
 let pravdepodobnost_vrece100_rovnake;
 let pravdepodobnost_vrece100_rozne;
 let pravdepodobnost_vrece100_lava_biela;
@@ -17,8 +21,10 @@ let pravdepodobnost_vrece100_prava_biela;
 let pravdepodobnost_vrece100_lava_cierna;
 let pravdepodobnost_vrece100_prava_cierna;
 
-let i = 0
-for (i = 0; i < 1000; i++) {
+let i = 0;
+const data_rozne_100 = [];
+const data_rozne_200 = [];
+for (i = 0; i < 10000; i++) {
   let gulicka1_vrece100 = vrece100.random();
   let gulicka2_vrece100 = vrece100.random();
   let gulicka1_vrece200 = vrece200.random();
@@ -28,29 +34,69 @@ for (i = 0; i < 1000; i++) {
     vrece100_rovnake++;
   } else {
     vrece100_rozne++;
+    
   }
-
 
   if (gulicka1_vrece200 === gulicka2_vrece200) {
     vrece200_rovnake++;
   } else {
     vrece200_rozne++;
   }
-
+  data_rozne_100.push({y:(vrece100_rozne / i) * 100,x:i});
+  data_rozne_200.push({y:(vrece200_rozne / i) * 100,x:i});
 }
-console.log("vreco100 " + vrece100_rovnake/i * 100+ " "+vrece100_rozne/i*100) ;
-console.log("vreco200 " + vrece200_rovnake/i * 100+ " "+vrece200_rozne/i*100) ;
+console.log(
+  'vreco100 ' + (vrece100_rovnake / i) * 100 + ' ' + (vrece100_rozne / i) * 100
+);
+console.log(
+  'vreco200 ' + (vrece200_rovnake / i) * 100 + ' ' + (vrece200_rozne / i) * 100
+);
+
+document.querySelector('#rovnake100').innerHTML = (vrece100_rovnake / i) * 100;
+document.querySelector('#rovnake200').innerHTML = (vrece200_rovnake / i) * 100;
+document.querySelector('#rozne100').innerHTML = (vrece100_rozne / i) * 100;
+document.querySelector('#rozne200').innerHTML = (vrece200_rozne / i) * 100;
+document.querySelector('#pocet').innerHTML = i;
 
 
-// Update header text
-document.querySelector('#rovnake100').innerHTML = vrece100_rovnake/i * 100
-document.querySelector('#rovnake200').innerHTML = vrece200_rovnake/i * 100
-document.querySelector('#rozne100').innerHTML = vrece100_rozne/i * 100
-document.querySelector('#rozne200').innerHTML = vrece200_rozne/i * 100
-document.querySelector('#pocet').innerHTML = i
+
+console.log(data_rozne_100)
 
 
 
 
-// Log to console
-//console.log(gulicka2_vrece100 )
+const data = {
+  datasets: [{
+    label: 'Vrece100',
+    data: data_rozne_100,
+    backgroundColor: 'red'
+  },
+  {
+    label: 'Vrece200',
+    data: data_rozne_200,
+    backgroundColor: 'blue'
+  }],
+};
+
+
+
+
+var gulicky = {
+  type: 'scatter',
+  data: data,
+  options: {
+    scales: {
+      x: {
+        type: 'linear',
+        position: 'bottom'
+      }
+    }
+  }
+}
+// get line chart canvas
+var gulickygraf = document.getElementById('gulickygraf').getContext('2d');
+// draw line chart
+new Chart(gulickygraf,gulicky);
+
+
+
